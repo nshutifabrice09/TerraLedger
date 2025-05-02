b3b8ab388c36b769499da5cb26a10c2f5836e4f0
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-02 13:42:31 +0200

    initial commit

---
+++ src/test/java/com/backend/terraLedger/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.terraLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
