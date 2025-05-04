a15f9e875d52c54ff165cb3d91b0a0e52500ef8f
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-04 10:03:45 +0200

    repository-updates

--- src/main/java/com/backend/terraLedger/model/SmartContractTransaction.java
+++ src/main/java/com/backend/terraLedger/model/SmartContractTransaction.java
@@ -1,4 +1,21 @@
 package com.backend.terraLedger.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "smart_contract_transaction")
 public class SmartContractTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String contractAddress;
+    private String transactionHash;
+    private String action; //"OwnershipTransfer", "LandRegistration"
+    private LocalDateTime timestamp;
+    private boolean success;
+    @ManyToOne
+    private LandParcel landParcel;
+
 }
