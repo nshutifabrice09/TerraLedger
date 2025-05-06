9aef9acb45b1909f20ba18f632becd602a47f40c
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 23:33:24 +0200

    exception-advice-updates

--- src/main/java/com/backend/terraLedger/exception/EvidenceNotFoundAdvice.java
+++ src/main/java/com/backend/terraLedger/exception/EvidenceNotFoundAdvice.java
@@ -1,2 +1,23 @@
-package com.backend.terraLedger.exception;public class EvidenceNotFoundAdvice {
+package com.backend.terraLedger.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class EvidenceNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(EvidenceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> exceptionHandler(EvidenceNotFoundException exception){
+        Map<String,String> errorMap=new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+        return errorMap;
+    }
 }
