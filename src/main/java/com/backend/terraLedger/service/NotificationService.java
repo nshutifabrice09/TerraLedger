e46f2a25ad95752487ccf4c8c03e769a0e230879
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 13:00:22 +0200

    serviceimplementations

--- src/main/java/com/backend/terraLedger/service/NotificationService.java
+++ src/main/java/com/backend/terraLedger/service/NotificationService.java
@@ -1,2 +1,14 @@
-package com.backend.terraLedger.service;public interface NotificationService {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.Notification;
+import org.aspectj.weaver.ast.Not;
+
+import java.util.List;
+
+public interface NotificationService {
+    List<Notification> getAllNotifications();
+    Notification getNotificationById(Long id);
+    Notification saveNotification(Notification notification);
+    Notification updateNotification(Long id, Notification notification);
+    void deleteNotificationById(Long id);
 }
--- src/main/java/com/backend/terraLedger/service/NotificationServiceImplementation.java
+++ src/main/java/com/backend/terraLedger/service/NotificationServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.terraLedger.service;public class NotificationServiceImplementation {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.Notification;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationServiceImplementation implements NotificationService{
+    @Override
+    public List<Notification> getAllNotifications() {
+        return null;
+    }
+
+    @Override
+    public Notification getNotificationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Notification saveNotification(Notification notification) {
+        return null;
+    }
+
+    @Override
+    public Notification updateNotification(Long id, Notification notification) {
+        return null;
+    }
+
+    @Override
+    public void deleteNotificationById(Long id) {
+
+    }
 }
