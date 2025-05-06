05f8ce87e1b0ea42ce9160be0cb32e0cf2517c52
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 12:46:32 +0200

    entities-updates-service

--- src/main/java/com/backend/terraLedger/service/DisputeService.java
+++ src/main/java/com/backend/terraLedger/service/DisputeService.java
@@ -1,2 +1,12 @@
-package com.backend.terraLedger.service;public interface DisputeService {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.Dispute;
+
+import java.util.List;
+
+public interface DisputeService {
+    List<Dispute> getAllDisputes();
+    Dispute getDisputeById(Long id);
+    Dispute saveDispute(Dispute dispute);
+    Dispute updateDispute(Long id, Dispute dispute);
 }
--- src/main/java/com/backend/terraLedger/service/DisputeServiceImplementation.java
+++ src/main/java/com/backend/terraLedger/service/DisputeServiceImplementation.java
@@ -1,2 +1,29 @@
-package com.backend.terraLedger.service;public class DisputeServiceImplementation {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.Dispute;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DisputeServiceImplementation implements DisputeService{
+    @Override
+    public List<Dispute> getAllDisputes() {
+        return null;
+    }
+
+    @Override
+    public Dispute getDisputeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Dispute saveDispute(Dispute dispute) {
+        return null;
+    }
+
+    @Override
+    public Dispute updateDispute(Long id, Dispute dispute) {
+        return null;
+    }
 }
