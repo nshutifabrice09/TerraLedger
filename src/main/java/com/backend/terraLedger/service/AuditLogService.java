05c39bed10ad04eb9b9ac4a0f73a700ecde15ecf
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 12:42:54 +0200

    service-implementaion

--- src/main/java/com/backend/terraLedger/service/AuditLogService.java
+++ src/main/java/com/backend/terraLedger/service/AuditLogService.java
@@ -1,5 +1,14 @@
 package com.backend.terraLedger.service;
 
 
+import com.backend.terraLedger.model.AuditLog;
+
+import java.util.List;
+
 public interface AuditLogService {
+    List<AuditLog> getAllAuditLogs();
+    AuditLog getAuiAuditLogById(Long id);
+    AuditLog saveAuditLog(AuditLog auditLog);
+    AuditLog updateAuditLog(Long id, AuditLog auditLog);
+    void deleteAuditLogById(Long id);
 }
--- src/main/java/com/backend/terraLedger/service/AuditLogServiceImplementation.java
+++ src/main/java/com/backend/terraLedger/service/AuditLogServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.terraLedger.service;public class AuditLogServiceImplementation {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.AuditLog;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuditLogServiceImplementation implements AuditLogService{
+    @Override
+    public List<AuditLog> getAllAuditLogs() {
+        return null;
+    }
+
+    @Override
+    public AuditLog getAuiAuditLogById(Long id) {
+        return null;
+    }
+
+    @Override
+    public AuditLog saveAuditLog(AuditLog auditLog) {
+        return null;
+    }
+
+    @Override
+    public AuditLog updateAuditLog(Long id, AuditLog auditLog) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuditLogById(Long id) {
+
+    }
 }
