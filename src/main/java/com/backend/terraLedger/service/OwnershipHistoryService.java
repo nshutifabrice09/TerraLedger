5767135ffa7a68c6921305e383e0f8a7036e1c27
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 13:02:44 +0200

    final-service-implementations

--- src/main/java/com/backend/terraLedger/service/OwnershipHistoryService.java
+++ src/main/java/com/backend/terraLedger/service/OwnershipHistoryService.java
@@ -1,2 +1,13 @@
-package com.backend.terraLedger.service;public interface OwnershipHistoryService {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.OwnershipHistory;
+
+import java.util.List;
+
+public interface OwnershipHistoryService {
+    List<OwnershipHistory> getAlOwnershipHistories();
+    OwnershipHistory getOwnershipHistoryById(Long id);
+    OwnershipHistory saveOwnershipHistory(OwnershipHistory ownershipHistory);
+    OwnershipHistory updateOwnershipHistory(Long id, OwnershipHistory ownershipHistory);
+    void deleteOwnershipHistoryById (Long id);
 }
--- src/main/java/com/backend/terraLedger/service/OwnershipHistoryServiceImplementation.java
+++ src/main/java/com/backend/terraLedger/service/OwnershipHistoryServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.terraLedger.service;public class OwnershipHistoryServiceImplementation {
+package com.backend.terraLedger.service;
+
+import com.backend.terraLedger.model.OwnershipHistory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OwnershipHistoryServiceImplementation implements OwnershipHistoryService{
+    @Override
+    public List<OwnershipHistory> getAlOwnershipHistories() {
+        return null;
+    }
+
+    @Override
+    public OwnershipHistory getOwnershipHistoryById(Long id) {
+        return null;
+    }
+
+    @Override
+    public OwnershipHistory saveOwnershipHistory(OwnershipHistory ownershipHistory) {
+        return null;
+    }
+
+    @Override
+    public OwnershipHistory updateOwnershipHistory(Long id, OwnershipHistory ownershipHistory) {
+        return null;
+    }
+
+    @Override
+    public void deleteOwnershipHistoryById(Long id) {
+
+    }
 }
