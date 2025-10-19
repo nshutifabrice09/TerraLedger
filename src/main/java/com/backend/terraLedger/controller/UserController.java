5727691bd05257ddce80e49db5d6a50e8e498c1b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-19 11:20:16 +0200

    initial userController commit

---
+++ src/main/java/com/backend/terraLedger/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.backend.terraLedger.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/user")
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+}
