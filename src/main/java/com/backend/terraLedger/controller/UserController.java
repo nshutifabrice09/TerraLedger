853edea952923298dd56d1e5197097a3a5e479a3
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-20 17:27:15 +0200

    initial userController commit

--- src/main/java/com/backend/terraLedger/controller/UserController.java
+++ src/main/java/com/backend/terraLedger/controller/UserController.java
@@ -1,11 +1,28 @@
 package com.backend.terraLedger.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.terraLedger.model.User;
+import com.backend.terraLedger.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/user")
+@RequestMapping("/api/")
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
+
+    private final UserService userService;
+
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User saveUser = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveUser);
+    }
 }
